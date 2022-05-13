d7b17061274f0556eab3ac95c9ea47b482f335e1
Guilherme Ignácio <stormierearth9@gmail.com>
2022-05-13 06:45:57 -0300

    Controle das áreas da tela

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+let seuVotoPara = document.querySelector(".d-1-left span");
+let cargo = document.querySelector(".cargo span");
+let descricao = document.querySelector(".dados");
+let aviso = document.querySelector(".d-2");
+let fotos = document.querySelector(".d-1-right");
+let numeros = document.querySelector(".num");
